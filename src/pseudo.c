773183fb2faedb683a8c91c10685771a7c5dbbe3
Kevin Cozens <kevin@ve3syb.ca>
2021-10-05 13:27:56 -0400

    Indentation changes to src/pseudo.c file

--- src/pseudo.c
+++ src/pseudo.c
@@ -51,132 +51,132 @@ int sizeof_pseudo(void)
  */
 void do_pseudo(int op /* which op */)
 {
-        char    fccdelim;
-        int     j;
-        int     fill;
-        char    *skip_white();
+    char    fccdelim;
+    int     j;
+    int     fill;
+    char    *skip_white();
 
-        if( op != EQU && *Label )
-                install(Label,Pc);
+    if( op != EQU && *Label )
+        install(Label,Pc);
 
-        P_force++;
-        switch(op){
-                case RMB:                       /* reserve memory bytes */
-                        if( eval() ){
-                                Pc +=  Result;
-                                f_record();     /* flush out bytes */
-                                }
-                        else
-                                error("Undefined Operand during Pass One");
-                        break;
-                case ZMB:                       /* zero memory bytes */
-                        if( eval() )
-                                while( Result-- )
-                                        emit(0);
-                        else
-                                error("Undefined Operand during Pass One");
-                        break;
-                case FILL:                      /* fill memory with constant */
-                        eval();
-                        fill = Result;
-                        if( *Optr++ != ',' )
-                                error("Bad fill");
-                        else{
-                                Optr = skip_white(Optr);
-                                eval();
-                                while( Result-- )
-                                        emit(fill);
-                                }
-                        break;
-                case FCB:                       /* form constant byte(s) */
-                        do{
-                                Optr = skip_white(Optr);
-                                eval();
-                                if( Result > 0xFF ){
-                                        if(!Force_byte)
-                                                warn("Value truncated");
-                                        Result = lobyte(Result);
-                                        }
-                                emit(Result);
-                        }while( *Optr++ == ',' );
-                        break;
-                case FDB:                       /* form double byte(s) */
-                        do{
-                                Optr = skip_white(Optr);
-                                eval();
-                                eword(Result);
-                        }while( *Optr++ == ',' );
-                        break;
-                case FCC:                       /* form constant characters */
-                        if(*Operand==EOS)
-                                break;
-                        fccdelim = *Optr++;
-                        while( *Optr != EOS && *Optr != fccdelim)
-                                emit(*Optr++);
-                        if(*Optr == fccdelim)
-                                Optr++;
-                        else
-                                error("Missing Delimiter");
-                        break;
-                case ORG:                       /* origin */
-                        if( eval() ){
-                                Old_pc = Pc = Result;
-                                f_record();     /* flush out any bytes */
-                                }
-                        else
-                                error("Undefined Operand during Pass One");
-                        break;
-                case EQU:                       /* equate */
-                        if(*Label==EOS){
-                                error("EQU requires label");
-                                break;
-                                }
-                        if( eval() ){
-                                install(Label,Result);
-                                Old_pc = Result;        /* override normal */
-                                }
-                        else
-                                error("Undefined Operand during Pass One");
-                        break;
-                case OPT:                       /* assembler option */
-                        P_force=0;
-                        if( head(Operand,"l") )
-                                Lflag=1;
-                        else if( head(Operand,"nol"))
-                                Lflag=0;
-                        else if( head(Operand,"c")){
-                                Cflag=1;
-                                Ctotal=0;
-                                }
-                        else if( head(Operand,"noc"))
-                                Cflag=0;
-                        else if( head(Operand,"contc")){
-                                Cflag=1;
-                                }
-                        else if ( head(Operand,"s"))
-                                Sflag = 1;
-                        else if ( head(Operand,"cre"))
-                                CREflag = 1;
-                        else
-                                error("Unrecognized OPT");
-                        break;
-                case PAGE:                      /* go to a new page */
-                        P_force=0;
-                        N_page = 1;
-                        if (Pass == 2 )
-                         if (Lflag)
-                          {
-                           printf ("\f");
-                           printf ("%-10s",Argv[Cfn]);
-                           printf ("                                   ");
-                           printf ("page %3d\n",Page_num++);
-                          }
-                        break;
-                case NULL_OP:                   /* ignored psuedo ops */
-                        P_force=0;
-                        break;
-                default:
-                        fatal("Pseudo error");
+    P_force++;
+    switch(op){
+        case RMB:               /* reserve memory bytes */
+            if( eval() ){
+                Pc +=  Result;
+                f_record();     /* flush out bytes */
                 }
+            else
+                error("Undefined Operand during Pass One");
+            break;
+        case ZMB:               /* zero memory bytes */
+            if( eval() )
+                while( Result-- )
+                    emit(0);
+            else
+                error("Undefined Operand during Pass One");
+            break;
+        case FILL:              /* fill memory with constant */
+            eval();
+            fill = Result;
+            if( *Optr++ != ',' )
+                error("Bad fill");
+            else{
+                Optr = skip_white(Optr);
+                eval();
+                while( Result-- )
+                    emit(fill);
+                }
+            break;
+        case FCB:               /* form constant byte(s) */
+            do{
+                Optr = skip_white(Optr);
+                eval();
+                if( Result > 0xFF ){
+                    if(!Force_byte)
+                        warn("Value truncated");
+                    Result = lobyte(Result);
+                    }
+                emit(Result);
+            }while( *Optr++ == ',' );
+            break;
+        case FDB:               /* form double byte(s) */
+            do{
+                Optr = skip_white(Optr);
+                eval();
+                eword(Result);
+            }while( *Optr++ == ',' );
+            break;
+        case FCC:               /* form constant characters */
+            if(*Operand==EOS)
+                break;
+            fccdelim = *Optr++;
+            while( *Optr != EOS && *Optr != fccdelim)
+                emit(*Optr++);
+            if(*Optr == fccdelim)
+                Optr++;
+            else
+                error("Missing Delimiter");
+            break;
+        case ORG:               /* origin */
+            if( eval() ){
+                Old_pc = Pc = Result;
+                f_record();     /* flush out any bytes */
+                }
+            else
+                error("Undefined Operand during Pass One");
+            break;
+        case EQU:               /* equate */
+            if(*Label==EOS){
+                error("EQU requires label");
+                break;
+                }
+            if( eval() ){
+                install(Label,Result);
+                Old_pc = Result;    /* override normal */
+                }
+            else
+                error("Undefined Operand during Pass One");
+            break;
+        case OPT:               /* assembler option */
+            P_force=0;
+            if( head(Operand,"l") )
+                Lflag=1;
+            else if( head(Operand,"nol"))
+                Lflag=0;
+            else if( head(Operand,"c")){
+                Cflag=1;
+                Ctotal=0;
+                }
+            else if( head(Operand,"noc"))
+                Cflag=0;
+            else if( head(Operand,"contc")){
+                Cflag=1;
+                }
+            else if ( head(Operand,"s"))
+                Sflag = 1;
+            else if ( head(Operand,"cre"))
+                CREflag = 1;
+            else
+                error("Unrecognized OPT");
+            break;
+        case PAGE:              /* go to a new page */
+            P_force=0;
+            N_page = 1;
+            if (Pass == 2 )
+             if (Lflag)
+              {
+               printf ("\f");
+               printf ("%-10s",Argv[Cfn]);
+               printf ("                   ");
+               printf ("page %3d\n",Page_num++);
+              }
+            break;
+        case NULL_OP:           /* ignored psuedo ops */
+            P_force=0;
+            break;
+        default:
+            fatal("Pseudo error");
+        }
 }
 
